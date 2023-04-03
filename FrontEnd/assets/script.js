26f2284bf7a3f13a11260c2b7d0c85afb76eee67
Nirina VELOSONINA <vnirina081@gmail.com>
2023-04-03 18:33:13 +0200

    afficher les images - modification script.js

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,26 +1,39 @@
-           
-    const portfolioUrl = 'http://localhost:5678/api/works';
-    let portfolios;
+let portfolioUrl = 'http://localhost:5678/api/works';
+let portfolios;
+
+
+
+fetch(portfolioUrl) 
+    .then(reponse => reponse.json())
+    .then(portfolios => {
+
+    showPortfolio(portfolios)
+    });
+
+function showPortfolio(datas){
+    for (i=0 ; i< datas.length; i++){
+
+        //création des éléments figure,img et figcaption
+        let galleryContainer = document.getElementsByClassName("gallery");
+        let figureElement = document.createElement('figure');
+        let imagesElement = document.createElement('img');
+        let figCapElement = document.createElement('figcaption');
+
+        //Afficher les images et les titres 
+        imagesElement.src = datas[i].imageUrl;
+        figCapElement.innerHTML = datas[i].title;
 
     
-    fetch(portfolioUrl) 
-        .then(reponse => reponse.json())
-        .then(portfolios => {
+        figureElement.appendChild(imagesElement);
+        figureElement.appendChild(figCapElement);
+        galleryContainer[0].appendChild(figureElement);
+    };
 
-        showPortfolio(portfolios)
-        }
-        );
-
-        function showPortfolio(datas){
-            let galleryContainer = document.querySelectorAll(".gallery");
-            let figureElement = document.createElement('figure');
-            let imagesElement = document.createElement('img');
-    
-            imagesElement.src = datas[0].imageUrl;
+}
+      
+       
         
-            figureElement.appendChild(imagesElement);
-
-
-        }
     
-        
+    
+
+   
