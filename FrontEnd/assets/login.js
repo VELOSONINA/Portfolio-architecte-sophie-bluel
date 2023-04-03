62336db45cb2b6472893f97a70047e953bf2ce2a
Nirina VELOSONINA <vnirina081@gmail.com>
2023-04-03 10:44:16 +0200

    ajout de login.html et login.js, modification du fichier index et script

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+   
+    let buttonConnexion = document.querySelector('#buttonClick');
+        buttonConnexion.addEventListener("click",function(){ 
+
+            let password = document.getElementById("password").value;
+            let eMail = document.getElementById("email").value;
+            // let request = require('request');
+            let url = 'http://localhost:5678/api/users/login';
+
+          
+        }
+    );
+}
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,15 +1,26 @@
-window.onload = function () {
-
-    let buttonConnexion = document.querySelector('buttonClick');
-        buttonConnexion.addEventListener("click",function(){ 
-            console.log("se connecter") 
-        });
-
+           
     const portfolioUrl = 'http://localhost:5678/api/works';
+    let portfolios;
+
     
-    fetch(portfolioUrl)
+    fetch(portfolioUrl) 
         .then(reponse => reponse.json())
-        .then(reponse => console.log(reponse));
+        .then(portfolios => {
+
+        showPortfolio(portfolios)
+        }
+        );
+
+        function showPortfolio(datas){
+            let galleryContainer = document.querySelectorAll(".gallery");
+            let figureElement = document.createElement('figure');
+            let imagesElement = document.createElement('img');
+    
+            imagesElement.src = datas[0].imageUrl;
+        
+            figureElement.appendChild(imagesElement);
 
 
-}
+        }
+    
+        
